103acc2abb7ebf6029eae1e2a992e1b27dd71216
sharonandisi <sharonandisi.28@gmail.com>
2019-01-07 05:07:09 +0300

    create a test for the profile class

--- zawadi/tests.py
+++ zawadi/tests.py
@@ -18,3 +18,19 @@ class ReviewTestClass(TestCase):
         self.image = Image(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/image',user=self.user)
         self.project.save()
         self.review = Review(id=1,image=self.image,design=1,usability=1,content=1,average=1,user=self.user)
+
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.sharon = Profile(first_name = 'Sharon', last_name= 'Andisi', username = 'migidza_andisi', bio = 'pray,love,laugh', email = 'sharonandisi.sa@gmail.com')
+
+        def test_instance(self):
+            self.assertTrue(isinstance(self.sharon,Profile)
+
+        def test_save_method(self):
+        self.sharon.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+        
