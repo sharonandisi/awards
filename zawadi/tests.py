29bf0defd65848871edb8d64a021a9c89a7952d9
sharonandisi <sharonandisi.28@gmail.com>
2019-01-07 05:02:56 +0300

    create test for the review class

--- zawadi/tests.py
+++ zawadi/tests.py
@@ -4,7 +4,17 @@ from django.test import TestCase
 class ImageTestClass(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='user', password='abc123')
-        self.image = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
+        self.image = Image(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/image',user=self.user)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.project,Project))
+
+class ReviewTestClass(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='abc123')
+        self.profile = Profile(profile_pic='images/pic', bio='eat pray love', contacts='+254720570175', user = self.user)
+        self.profile.save()
+        self.image = Image(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/image',user=self.user)
+        self.project.save()
+        self.review = Review(id=1,image=self.image,design=1,usability=1,content=1,average=1,user=self.user)
