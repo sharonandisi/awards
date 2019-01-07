b471bc8a4b973459564f980f4c161ce57df8ecd1
sharonandisi <sharonandisi.28@gmail.com>
2019-01-07 04:58:23 +0300

    creeate test for the image class

--- zawadi/tests.py
+++ zawadi/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='abc123')
+        self.image = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))
