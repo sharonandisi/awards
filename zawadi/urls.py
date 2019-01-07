de5521cf06d32ee95cc75e49e8b2622f5eea4a46
sharonandisi <sharonandisi.28@gmail.com>
2019-01-07 04:53:59 +0300

    since we added new view functions we now update the url patterns in accordance to the added view functions

--- zawadi/urls.py
+++ zawadi/urls.py
@@ -4,5 +4,16 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$',views.index, name='index'),
+    url(r'^$',views.index, name='index'),
+    url(r'^projects/(\d+)$', views.image, name='image'),
+    url(r'^upload/$', views.new_image, name='upload'),
+    url(r'^profile/(?P<username>\w{0,50})/$', views.profile, name='profile'),
+    url(r'^profile_edit/(\d+)$', views.profile_edit, name='edit_profile'),
+    url(r'^search/$', views.search_results, name='search_results'),
+    url(r'^api/profiles/$', views.ListProfiles.as_view()),
+    url(r'^api/projects/$', views.ListImages.as_view()),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
