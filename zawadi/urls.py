22f23a6df0a639541d78a6f8791419692e4443df
sharonandisi <sharonandisi.28@gmail.com>
2019-01-07 08:21:59 +0300

    create a base.html file

--- zawadi/urls.py
+++ zawadi/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index, name='index'),
     url(r'^projects/(\d+)$', views.image, name='image'),
-    url(r'^upload/$', views.new_image, name='upload'),
+    url(r'^new-image/$', views.new_image, name='new-image'),
     url(r'^profile/(?P<username>\w{0,50})/$', views.profile, name='profile'),
     url(r'^profile_edit/(\d+)$', views.profile_edit, name='edit_profile'),
     url(r'^search/$', views.search_results, name='search_results'),
